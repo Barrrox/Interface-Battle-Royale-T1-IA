b3fc06a70049c81ed5db2ed2af4182ec1610fd02
Barrrox <matheusbarros173@gmail.com>
2025-06-27 12:46:12 -0300

    comentarios

--- parametros.py
+++ parametros.py
@@ -2,20 +2,19 @@
 Arquivo para guardar variáveis globais e parâmetros utilizados nos códigos da pasta
 
 """
-
 from gerador_labirinto import gerar_labirinto_kruskal
 
+
+# Configurações de imagem
 LARGURA_TELA = 1290  # Múltiplo de 3 para divisão exata
 ALTURA_TELA = 821
 
 MARGEM_ESQUERDA = 20
 
-
+# Configurações do labirinto
 LARGURA_LABIRINTO = 60
 ALTURA_LABIRINTO = 60
-
 TAMANHO_CELULA = 3
-
 
 CAMINHO = 0
 PAREDE = 1
@@ -24,13 +23,16 @@ FIM = 3
 
 LABIRINTO_GLOBAL = gerar_labirinto_kruskal(LARGURA_LABIRINTO, ALTURA_LABIRINTO)
 
+# PONTO INICIAL
 PONTO_INICIAL = (1,0)
 
+# PONTO FINAL NA PAREDE INFERIOR
 for i in range(len(LABIRINTO_GLOBAL)):
     if LABIRINTO_GLOBAL[i][-1] == FIM: # procura nas linhas finais
         PONTO_FINAL = (i, len(LABIRINTO_GLOBAL)-1)
         print(PONTO_FINAL)
 
+# PONTO FINAL NA PAREDE DA DIREITA
 for i in range(len(LABIRINTO_GLOBAL[0])):
     if LABIRINTO_GLOBAL[-1][i] == FIM: # procura nas linhas finais
         PONTO_FINAL = (len(LABIRINTO_GLOBAL)-1, i)
